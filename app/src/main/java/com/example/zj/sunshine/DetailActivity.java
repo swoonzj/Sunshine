b6455a618c193494f12103f5914fc47032e69abb
swoonzj <swoonzj@verizon.net>
2016-11-24 11:44:21 -0500

    SettingsActivity works
    SettingsActivity can be accessed via options menu in MainActivity and
    DetailActivity.
    
    Location setting can be changed, but does nothing.

--- app/src/main/java/com/example/zj/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/zj/sunshine/DetailActivity.java
@@ -37,7 +37,7 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -59,6 +59,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
@@ -68,9 +69,9 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public DetailFragment() {
         }
 
         @Override
