cd7efc60e8dc964ec6c088bcc985cc3c2daeeb8a
swoonzj <swoonzj@verizon.net>
2017-02-13 14:36:24 -0500

    View Location In Map App
    Added menu option to show the user's chosen location (zip code) in the
    system's default Map application, via ACTION_VIEW intent.

--- app/src/main/java/com/example/zj/sunshine/MainActivity.java
+++ app/src/main/java/com/example/zj/sunshine/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.zj.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +41,29 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        // Open location on map, via map Intent
+        if (id == R.id.action_map){
+            openLocationInMap();
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    // Shows the current Location preference in the default system Map application (if one exists)
+    void openLocationInMap(){
+        // Get location preference
+        SharedPreferences prefs = getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        // Build location query URI
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        mapIntent.setData(geoLocation);
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
     }
 }
